321bedf48813b8bde93a148a8f10bc37c0d0e023
mrrayhan640 <151461906+mrrayhan640@users.noreply.github.com>
2025-07-26 22:24:20 -0700

    infoCommands.py

--- cogs/infoCommands.py
+++ cogs/infoCommands.py
@@ -257,7 +257,7 @@ class InfoCommands(commands.Cog):
 
 
 
-            embed.set_footer(text="DEVELOPED BY THUG")
+            embed.set_footer(text="DEVELOPED BY @legend_of_zooo")
             await ctx.send(embed=embed)
 
             if region and uid:
